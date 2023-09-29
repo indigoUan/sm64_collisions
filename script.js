5c060e5988576f818fdf4448df3521d56ec02951
îndigoUán <indigoUan@gmail.com>
2023-09-29 20:45:16 +0200

    added quarter steps. added Mario's moustache

--- script.js
+++ script.js
@@ -1,52 +1,47 @@
+let stepsize = 36;
 let speed = 1;
 
-var first = true;
-var oldX = 477;
+let lastpos = 477 + 36;
 
 function nextframe() {
 	let mario = document.getElementById("marioSPR");
-	let hitboxG = document.getElementById("marioBOXg");
-	let hitboxB = document.getElementById("marioBOXb");
+	let steps = [ document.getElementById("marioStep1"), document.getElementById("marioStep2"), document.getElementById("marioStep3"), document.getElementById("marioStep4") ];
 
-	var x = oldX - speed * 40 * (first? 0 : 1);
-	if (hitboxB.style.visibility == "unset") {
-		x = 150;
+	let x = lastpos;
+
+	let brokeout = 4;
+	for (let i = 0; i < steps.length; i++) {
+		let stepX = x - (i + 1) * (stepsize * 0.25) * speed;
+		steps[i].style.left = stepX + "px";
+		steps[i].style.opacity = 0.2 + 0.1 * i;
+		if (checkWallOverlap(stepX)) {
+			brokeout = i;
+			break;
+		}
+		lastpos = stepX;
 	}
-	var xHB = x - speed * 40;
-	var y = 38;
-
-	hitboxB.style.visibility = "hidden";
+	for (let i = brokeout; i < steps.length; i++) {
+		steps[i].style.opacity = 0.0;
+	}
 
 	mario.style.left = x + "px";
-	mario.style.top = y + "px";
-
-	hitboxB.style.left = hitboxG.style.left = xHB + "px";
-	hitboxB.style.top = hitboxG.style.top = y + "px";
-
-	if (checkWallOverlap(xHB)) {
-		hitboxB.style.visibility = "unset";
-	}
-
-	first = false;
-	oldX = x;
 }
 
-function changeSpeed(changeBy) {
-	oldX = 477;
-	first = true;
-	speed += changeBy;
+function changeSpeed(changeTo) {
+	lastpos = 477;
+	speed = changeTo;
 	if (speed <= 1) {
 		speed = 1;
 	}
-	if (speed >= 11) {
-		speed = 11;
+	if (speed >= 16) {
+		speed = 16;
 	}
 	nextframe();
 }
 
 function checkWallOverlap(x) {
 	let linex = x;
-	let linew = x + 62;
+	let linew = x + 46;
 	let linei = linex + (linew - linex) * 0.5;
 
 	let wallx = 100;
@@ -67,9 +62,19 @@ if (urlParams.has("initialspeed")) {
 	if (speed <= 1 || isNaN(speed)) {
 		speed = 1;
 	}
-	if (speed >= 11) {
-		speed = 11;
+	if (speed >= 16) {
+		speed = 16;
 	}
+}
+if (urlParams.has("stepsize")) {
+	stepsize = parseFloat(urlParams.get("stepsize"));
+	if (stepsize <= 1 || isNaN(speed)) {
+		stepsize = 1;
+	}
+	if (stepsize >= 400) {
+		stepsize = 400;
+	}
+	lastpos = 477 + stepsize;
 }
 
 nextframe();
